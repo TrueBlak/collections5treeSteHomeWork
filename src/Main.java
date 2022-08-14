2810c552ad4ecebc7965fd866af7d0a72116c8ba
Alexey Panfilov <ap83@list.ru>
2022-08-14 16:35:53 +0300

    Release version

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> persons = new ArrayList<>();
-        persons.add(new Person("Kolya", "Fuckin", 12));
-        persons.add(new Person("Jay", "Le Dolboeub", 28));
-        persons.add(new Person("Zakon", "Gay Lussuck Odin", 666));
-        persons.add(new Person("Gogy", "Gogua", 20));
-        persons.add(new Person("Vlad", "Tepes Ibn Faruh", 2550));
+        persons.add(new Person("Kolya", "Ivanov", 12));
+        persons.add(new Person("Andrey", "Ivanov Dyatlov", 28));
+        persons.add(new Person("Audi", "A8 Long Edition", 6666));
+        persons.add(new Person("Vasya", "Petrov", 20));
+        persons.add(new Person("Vlad", "Tepes Mercedes Le Dracula", 2550));
         persons.add(new Person("Andy", "Derris Hansen Lande Sammet", 999));
-        Collections.sort(persons, new PersonsSurnameWordsNumberComparator(3));
+        Collections.sort(persons, new PersonsSurnameWordsNumberComparator(1));
         for (Person p : persons) {
             System.out.println(p);
         }
