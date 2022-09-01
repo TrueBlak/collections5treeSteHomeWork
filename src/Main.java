479219fbd21f197a44e4de31a5f7738989fa9654
Alexey Panfilov <ap83@list.ru>
2022-09-01 11:42:33 +0300

    Predicate added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +12,10 @@ public class Main {
         persons.add(new Person("Vasya", "Petrov", 20));
         persons.add(new Person("Vlad", "Tepes Mercedes Le Dracula", 2550));
         persons.add(new Person("Andy", "Derris Hansen Lande Sammet", 999));
-        Collections.sort(persons, new PersonsSurnameWordsNumberComparator(1));
+        persons.add(new Person("Nikolay", "Ivanov Petrov Sidorov", 17));
+        Predicate<Person> personPredicate = person -> person.getAge() < 18;
+        persons.removeIf(personPredicate);
+        Collections.sort(persons, new PersonsSurnameWordsNumberComparator());
         for (Person p : persons) {
             System.out.println(p);
         }
